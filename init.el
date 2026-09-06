53f8001082f138678f1c4fadb36d314b839aa3aa
Data Mike <mjp35@cornell.edu>
2026-09-05 21:52:57 -0400

    Change all-the-icons to nerd-icons

--- init.el
+++ init.el
@@ -285,7 +285,8 @@ It should only modify the values of Spacemacs settings."
                                :width normal)
 
    ;; Default icons font, it can be `all-the-icons' or `nerd-icons'.
-   dotspacemacs-default-icons-font 'all-the-icons
+   ;; dotspacemacs-default-icons-font 'all-the-icons
+   dotspacemacs-default-icons-font 'nerd-icons
 
    ;; The leader key (default "SPC")
    dotspacemacs-leader-key "SPC"
