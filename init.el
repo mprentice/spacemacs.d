ca554337d9c1ed5dfcc05f8ff88fa836e14390d1
Data Mike <mjp35@cornell.edu>
2026-09-05 21:52:38 -0400

    Add a68-mode to additional packages

--- init.el
+++ init.el
@@ -109,7 +109,8 @@ This function should only modify configuration layer settings."
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
    dotspacemacs-additional-packages '(org-reverse-datetree
-                                      sqlite3)
+                                      sqlite3
+                                      a68-mode)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
